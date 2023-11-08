afaada08305ccb1140dfdc7940f1d96df6c7fb5d
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-11-08 17:43:30 -0600

    contact section and footer component

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Hero from './components/Hero'
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
@@ -11,7 +13,9 @@ export default function Home() {
         <Hero />
         <About />
         <Projects />
+        <Contact />
       </div>
+      <Footer />
     </main>
   );
 }
