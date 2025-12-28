16e997e47ad1a4c8cdc03793194b6c549b5f5cd3
Chris Boydstun <chrisboydstun@gmail.com>
2025-12-28 14:34:04 -0600

    fix: lazy load resend api key

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,9 +2,9 @@ import { EmailTemplate } from '../../components/EmailTemplate';
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req) {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    
     /* data from client */
     const body = await req.json();
 
